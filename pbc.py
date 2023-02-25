dde2d8f027857b9416794da94ce3b593481a0a93
slyrp <75110055+slyrp@users.noreply.github.com>
2023-02-25 00:20:22 -0500

    Main change (there's many): added code for config
    Config.json: config.json is a config file that is currently only used for .pbc and Images values. "pbc" will scan every directory for a .pbc file ("pbc": "directory, directory, directory"). Images will save to every directory listed. Kind of useless but whatever, cool for extras I guess.
    
    Removed input colors, Removed image note as it is incorrect, moved the checking of folders out of the createimage() function, fixed the issue where if you didn't create an image it would say the directory is incorrect, and some extra fixes/changes.

--- pbc.py
+++ pbc.py
@@ -6,17 +6,18 @@ from PIL import Image
 from colorama import Fore, Style, init
 import random
 import base64
+import json
 
 # storing random stuff for when it runs
 try:
     init()
 except Exception as e:
-    input(Fore.RED + "There was an error while initializing command prompt colors. Error: " + str(e) + Style.RESET_ALL)
+    input("There was an error while initializing command prompt colors. Error: " + str(e))
 try:
     cd = os.getcwd()
     da = ""
 except Exception as e:
-    input(Fore.RED + "There was an error while getting the current directory. Error: " + str(e) + Style.RESET_ALL)
+    input("There was an error while getting the current directory. Error: " + str(e))
 try:
     emu = [
         "Uh-oh! ",
@@ -39,27 +40,89 @@ try:
     ]
     gm = random.choice(gmu)
 except Exception as e:
-    input(Fore.RED + "Error while initializing good/bad dictionary. Error: " + str(e) + Style.RESET_ALL)
+    input("Error while initializing good/bad dictionary. Error: " + str(e))
+try:
+    if not os.path.exists(cd + "/config.json"):
+        cf = input(em + 'Looks like the "cd/config.json" directory does not exist. Would you like to create it? (y/n): ')
+        if cf == "y":
+            with open("config.json", "w") as w:
+                w.write("""
+{
+    "pbc": "imgs/",
+    "imgs": "imgs/"
+}
+                        """)
+
+            print(Fore.GREEN + gm + "Directory created successfully." + Style.RESET_ALL)
+            ce = True
+        else:
+            input(em + "Directory not created.")
+            exit()
+    else:
+        ce = True
+except Exception as e:
+    input('Error while checking for "cd/pbc" directory. Error: ' + str(e))
+
+with open("config.json", "r+") as rc:
+    try:
+        config = json.load(rc)
+        pbcp = config["pbc"]
+        pbcpaths = pbcp.split(", ")  # split the string into separate paths
+
+        pbcpaths = [pbc + "/" if not pbc.endswith("/") else pbc for pbc in pbcpaths]
+    except ValueError:
+        if "\\" in pbcp:
+            print(f"{Fore.YELLOW} Backslashes cannot be used in paths, replace them with forwardslashes {Style.RESET_ALL}")
+        input("cd value incorrect in config.json. Please make sure it is seperated with commas and shows correct paths. ")
+        exit()
+
+    try:
+        imgsp = config["imgs"]
+        imgspaths = imgsp.split(", ")  # split the string into separate paths
+
+        imgspaths = [imgs + "/" if not imgs.endswith("/") else imgs for imgs in imgspaths]
+        print(imgsp)
+        print(pbcp)
+    except ValueError:
+        input("imgs value incorrect in config.json. Please make sure it is seperated with commas and shows correct paths. ")
+        exit()
 
 
 # telling the user stuff, happens on every run
 print(Fore.CYAN + base64.b64decode(b'TWFkZSBieSBzbHlycCAoaHR0cHM6Ly9naXRodWIuY29tL3NseXJwL0FDTkgpICYgaGFsZi1zdG9sZW4gZnJvbSBUcmVla2kgKGh0dHBzOi8vZ2l0aHViLmNvbS9UcmVla2kvQ3lsaW5kcmljYWxFYXJ0aCk=').decode('utf-8') + Style.RESET_ALL)
-print("Note: any .pbc or .zs files should be stored in cd/pbc for it to work!")
-print("Note: for images, light grey is unwalkable, dark grey is walkable.")
+print(f"Note: any .pbc or .zs files should be stored in the paths in the config for it to work!{Fore.YELLOW} YOU MUST INCLUDE THE FULL PATH! {Style.RESET_ALL}")
 print("Current directory is: " + cd + '. If this is wrong open it by double-clicking instead of using "Open With."')
+if ce == True:
+    print(Fore.GREEN + gm + '"config.json" directory already exists.' + Style.RESET_ALL)
 try:
-    if not os.path.exists(cd + "/pbc"):
-        cf = input(Fore.RED + em + 'Looks like the "cd/pbc" directory does not exist. Would you like to create it? (y/n): ' + Style.RESET_ALL)
-        if cf == "y":
-            os.mkdir("pbc")
-            print(Fore.GREEN + gm + "Directory created successfully." + Style.RESET_ALL)
+    for pbcp in pbcpaths:
+        if not os.path.exists(pbcp):
+            cf = input(em + f'Looks like the "{pbcp}" directory does not exist. Would you like to create it? (y/n): ')
+            if cf == "y":
+                os.mkdir(pbcp)
+                print(Fore.GREEN + gm + "Directory created successfully." + Style.RESET_ALL)
+            else:
+                input(em + "Directory not created.")
+                exit()
         else:
-            input(Fore.RED + em + "Directory not created." + Style.RESET_ALL)
-            exit()
-    else:
-        print(Fore.GREEN + gm + '"/pbc" directory already exists.' + Style.RESET_ALL)
+            print(Fore.GREEN + gm + f'"{pbcp}" directory already exists.' + Style.RESET_ALL)
 except Exception as e:
-    input(Fore.RED + 'Error while checking for "cd/pbc" directory. Error: ' + str(e) + Style.RESET_ALL)
+    input(+ f'Error while checking for "{pbcp}" directory. Error: ' + str(e))
+
+try:
+    for imgsp in imgspaths:
+        if not os.path.exists(imgsp):
+            cf = input(em + f"Looks like the '{imgsp}' directory does not exist. Would you like to create it? (y/n): ")
+            if cf == "y":
+                os.mkdir(imgsp)
+                print(Fore.GREEN + gm + "Directory created successfully" + Style.RESET_ALL)
+            else:
+                input(em + "Directory not created")
+                exit()
+        else:
+            print(Fore.GREEN + f"Good, '{imgsp}' directory already exists" + Style.RESET_ALL)
+except Exception as e:
+    input(f'Error while checking for "{imgsp}" directory. Error: ' + str(e))
 
 
 #starts off by asking for file name
@@ -69,18 +132,6 @@ name = input("Please enter file name (.zs/.pbc): ")
 def createimage(name, blob, w, h, da):
     ci = input("Would you like to create an image? (y/n): ")
     if ci == "y":
-        try:
-            if not os.path.exists(cd + "/imgs"):
-                cf = input(Fore.RED + em + "Looks like the 'cd/imgs' directory does not exist. Would you like to create it? (y/n): " + Style.RESET_ALL)
-                if cf == "y":
-                    os.mkdir("imgs")
-                    print(Fore.GREEN + gm + "Directory created successfully" + Style.RESET_ALL)
-                else:
-                    print(Fore.RED + em + "Directory not created" + Style.RESET_ALL)
-            else:
-                print(Fore.GREEN + "Good, '/imgs' directory already exists" + Style.RESET_ALL)
-        except Exception as e:
-            input('Error while checking for "cd/imgs" directory. Error: ' + str(e))
         try:
             sets = [set() for i in range(12)]
             img = Image.new('RGBA', (w*2, h*2))
@@ -100,19 +151,20 @@ def createimage(name, blob, w, h, da):
                     pix[x*2+1,y*2+1] = (c,c,c,255)
                     pix[x*2+1,y*2] = (d,d,d,255)
                     offset += 0x34
-            img.save('imgs/%s.png' % name)
+            for eyes in imgspaths:
+                try:
+                    img.save(f"{eyes}/{name}.png")
+                    print(Fore.GREEN + f"Saving image to: {eyes}/{name}.png" + Style.RESET_ALL)
+                    print(Fore.GREEN + gm + "The file was saved successfully." + Style.RESET_ALL)
+                except Exception as e:
+                    print(Fore.RED + em + "There was a problem saving the image. Error: " + str(e) + ". Let's try that again." + Style.RESET_ALL)
+                    name = input("Please enter file name (.zs/.pbc): ")
+                    checkname(name)
             for s in sets:
                 print(s)
                 print(min(s), max(s))
         except Exception as e:
-            print(Fore.RED + em + "Your image couldn't be saved. Error: " + str(e) + ". Let's try that again." + Style.RESET_ALL)
-            name = input("Please enter file name (.zs/.pbc): ")
-            zs(name)
-        try:
-            print(Fore.GREEN + 'Saving image to:', 'imgs/%s.png' % name + Style.RESET_ALL)
-            print(Fore.GREEN + gm + "The file was saved successfully." + Style.RESET_ALL)
-        except Exception as e:
-            print(Fore.RED + em + "There was a problem saving the image. Error: " + str(e) + ". Let's try that again." + Style.RESET_ALL)
+            print(Fore.RED + em + "The image couldn't be created. Error: " + str(e) + ". Let's try that again." + Style.RESET_ALL)
             name = input("Please enter file name (.zs/.pbc): ")
             zs(name)
         da = input("Would you like to decompress another .zs/.pbc? (y/n): ")
@@ -123,23 +175,29 @@ def createimage(name, blob, w, h, da):
             exit()
     elif ci == "n":
         try:
-            name = input("Would you like to decompress another .zs/.pbc? (y/n): ")
+            da = input("Would you like to decompress another .zs/.pbc? (y/n): ")
             if da == "y":
                 name = input("Please enter file name (.zs/.pbc): ")
             checkname(name)
         except Exception as e:
-            print(Fore.RED + "Error while initializing code to decompress another file. Error: " + e)
+            print(Fore.RED + "Error while initializing code to decompress another file. Error: " + e + ". Let's try that again" + Style.RESET_ALL)
+            name = input("Please enter file name (.zs/.pbc): ")
+            checkname(name)
     else:
         exit()
 
 # for zs files
 def zs(name):
-    try:
-        blob = open('pbc/' + name, 'rb').read()
-    except:
-        print(Fore.RED + em + "There was a problem opening that file, maybe the file doesn't exist? Let's try that again." + Style.RESET_ALL)
+    for pbcp in pbcpaths:
+        try:
+            blob = open(pbcp + name, 'rb').read()
+            break  # Exit the loop if the file is found
+        except FileNotFoundError:
+            pass  # Try the next path if the file is not found
+    else:
+        print(Fore.RED + em + "That file doesn't exist. Let's try that again." + Style.RESET_ALL)
         name = input("Please enter file name (.zs/.pbc): ")
-        zs(name)
+        checkname(name)
     try:
         blob = zstandard.ZstdDecompressor().decompress(blob)
         arc = sarc.SARC(blob)
@@ -171,14 +229,19 @@ def zs(name):
         createimage(name, blob, w, h, da)
     except Exception as e:
         print(Fore.RED + 'Error while initiating "createimage(). Error: ' + str(e))
+        exit()
 
 
 
 # for pbc files
 def pbc(name):
-    try:
-        blob = open('pbc/' + name, 'rb').read()
-    except:
+    for pbcp in pbcpaths:
+        try:
+            blob = open(pbcp + name, 'rb').read()
+            break  # Exit the loop if the file is found
+        except FileNotFoundError:
+            pass  # Try the next path if the file is not found
+    else:
         print(Fore.RED + em + "That file doesn't exist. Let's try that again." + Style.RESET_ALL)
         name = input("Please enter file name (.zs/.pbc): ")
         pbc(name)
